61676c54e1df763c7e392e473c0f3bb41eae58f6
Johnny <jim850221@gmail.com>
2021-08-15 22:39:52 +0800

    first commit

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,7 @@
+// JSX
+var template = /*#__PURE__*/React.createElement("p", {
+    id: "app"
+  }, "This is JSX");
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
